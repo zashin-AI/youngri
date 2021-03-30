8ca6039c089e116331098ba1a86b3b4ee94092d7
YoungriKIM <lemontleo0311@gmail.com>
2021-03-30 16:29:08 +0900

    2021-03-30

--- librosa/bbang_01.py
+++ librosa/bbang_01.py
@@ -12,9 +12,13 @@ import librosa
 
 # y: 소리가 떨리는 세기(진폭)를 시간 순서대로 나열한 것
 # Sampling rate(sr): 1초당 샘플의 개수, 단위 Hz 또는 kHz
-y, sr = librosa.load('C:/nmd/data/pansori/7KCV6riw7KCV/GJu8ZETMTZU/7KCV6riw7KCV-GJu8ZETMTZU-0036.flac'\
+y, sr = librosa.load('C:/nmb/data/pansori/7KCV6riw7KCV/GJu8ZETMTZU/7KCV6riw7KCV-GJu8ZETMTZU-0036.flac'\
                     , sr=22050) # sr default 값이 22050이다.
                                 # 왜?? > https://m.blog.naver.com/PostView.nhn?blogId=satusfree5&logNo=220623885225&proxyReferer=https:%2F%2Fwww.google.com%2F
+
+
+# 데이터 음성 성별: 남성
+# 문구 : 특히나 이 공식은 매우 익숙해 있는거죠
 
 print(y)
 print(len(y))
@@ -140,4 +144,70 @@ plt.legend(['y_harm', 'y_perc'])
 # 4) Spectral Centroid
 # 소리를 주파수 표현했을 때, 주파수의 가중평균을 계산하여 소리의 "무게 중심"이 어딘지를 알려주는 지표
 # 예를 들어, 블루스 음악은 무게 중심이 가운데 부분에 놓여있는 반면, 메탈 음악은 (끝 부분에서 달리기 때문에) 노래의 마지막 부분에 무게 중심이 실린다
+
+spectral_centroids = librosa.feature.spectral_centroid(y, sr=sr)[0]
+
+# 시각화를위한 시간 변수 계산
+frames = range(len(spectral_centroids))
+
+# 프레임 수를 시간(초)으로 변환
+t = librosa.frames_to_time(frames)
+
+import sklearn
+def normalize(x, axis=0):
+    return sklearn.preprocessing.minmax_scale(x, axis=axis)
+
+plt.figure(figsize=(10, 4))
+plt.title('Harmonic and Percussive Components')
+librosa.display.waveplot(y, sr=sr, alpha=0.5, color='b')
+plt.plot(t, normalize(spectral_centroids), color='r')
+plt.legend(['y', 't'])
+# plt.show()
+
+# ====================================
+# 5) Spectral Rolloff(굴러 떨어지다)
+# 신호 모양을 측정한다
+# 총 스펙트럴 에너지 중 낮은 주파수(85% 이하)에 얼마나 많이 집중되어 있는가
+
+spectral_rolloff = librosa.feature.spectral_rolloff(y, sr=sr)[0]
+
+plt.figure(figsize=(10, 4))
+plt.title('Spectral Rolloff')
+librosa.display.waveplot(y, sr=sr, alpha=0.5, color='b')
+plt.plot(t, normalize(spectral_rolloff), color='r')
+plt.legend(['y', 't'])
+# plt.show()
+
+# ====================================
+# 6) Mel-Frequency Cepstral Coefficients (MFCCs)
+# MFCCs는 특징들의 작은 집합(약 10-20)으로 스펙트럴 포곡선의 전체적인 모양을 축약하여 보여준다
+# 사람의 청각 구조를 반영하여 음성 정보 추출
+# https://tech.kakaoenterprise.com/66
+
+mfccs = librosa.feature.mfcc(y, sr=sr)
+mfccs = normalize(mfccs, axis=1)
+
+print('mean: %.2f' % mfccs.mean())
+print('var: %.2f' % mfccs.var())
+
+plt.figure(figsize=(10,4))
+plt.title('MFCCs')
+librosa.display.specshow(mfccs, sr=sr, x_axis='time')
+plt.colorbar()
+# plt.show()
+
+# ====================================
+# 7) Chroma Frequencies
+# 크로마 특징은 음악의 흥미롭고 강렬한 표현이다
+# 크로마는 인간 청각이 옥타브 차이가 나는 주파수를 가진 두 음을 유사음으로 인지한다는 음악이론에 기반한다
+# 모든 스펙트럼을 12개의 Bin으로 표현한다
+# 12개의 Bin은 옥타브에서 12개의 각기 다른 반음(Semitones=Chroma)을 의미한다
+
+chromagram = librosa.feature.chroma_stft(y, sr=sr, hop_length=512)
+
+plt.figure(figsize=(10,4))
+plt.title('Chroma Frequencies')
+librosa.display.specshow(chromagram, x_axis='time', y_axis='chroma', hop_length=512)
+plt.colorbar()
+plt.show()
 
