93a9b3e3d58fdd2c8da43b3b4aefd5f17b6137c7
YoungriKIM <lemontleo0311@gmail.com>
2021-04-17 12:31:12 +0900

    2021-04-17

--- python_import/test_audio_sum_use_02.py
+++ python_import/test_audio_sum_use_02.py
@@ -53,14 +53,14 @@ for file in infiles:
 
 # --------------------------------------------------------------
 # 적용해보자!
-audio_dir = 'C:/Users/Admin/Desktop/M_sum/'
+audio_dir = 'C:/Users/Admin/Desktop/new/'
 infiles = librosa.util.find_files(audio_dir)
 
 count = 0
 for file in infiles:
     origin_dir = infiles[count]
     threshold = 5000
-    out_dir = 'C:/Users/Admin/Desktop/M_1m/'
+    out_dir = 'C:/Users/Admin/Desktop/new_1m/'
     end_threshold = 60000
     voice_split_1m(origin_dir=origin_dir, threshold=threshold, end_threshold =end_threshold,out_dir=out_dir)
     count += 1
