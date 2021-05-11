11f149d58e5d209fb1dece7801cef1b1f658ac3d
YoungriKIM <lemontleo0311@gmail.com>
2021-05-11 14:16:04 +0900

    2021-05-11

--- python_import/noise_handling.py
+++ python_import/noise_handling.py
@@ -57,5 +57,5 @@ def denoise_tim(
                     hop_length=hop_length,
                     win_length=win_length)
 
-                sf.write(out_dir + ext_dir + filename[:-4] + '_denoise.wav', data, sr) # 노이즈 제거 한 파일 생성
+                sf.write(out_dir + ext_dir + filename[:-4] + '_denoise.wav', reduce_noise, sr) # 노이즈 제거 한 파일 생성
                 print("%s/%s" % (path, filename) + ' done') # 완료 된 경우에 출력
